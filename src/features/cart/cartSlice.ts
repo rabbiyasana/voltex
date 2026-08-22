e32ba2533c542e7ceda7858531915785b347b316
rabbiyasana <64980654+rabbiyasana@users.noreply.github.com>
2026-08-23 00:46:02 +0500

    cart slice

---
+++ src/features/cart/cartSlice.ts
@@ -0,0 +1,102 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+import type { Product } from "../products/productType";
+
+interface CartItem {
+  product: Product;
+  quantity: number;
+}
+
+interface CartState {
+  items: CartItem[];
+}
+
+const initialState: CartState = {
+  items: [],
+};
+
+interface AddToCartPayload {
+  product: Product;
+  quantity: number;
+}
+
+const cartSlice = createSlice({
+  name: "cart",
+
+  initialState,
+
+  reducers: {
+    addToCart: (
+      state,
+      action: PayloadAction<AddToCartPayload>
+    ) => {
+      const { product, quantity } = action.payload;
+
+      const existingItem = state.items.find(
+        (item) => item.product.id === product.id
+      );
+
+      if (existingItem) {
+        existingItem.quantity += quantity;
+      } else {
+        state.items.push({
+          product,
+          quantity,
+        });
+      }
+    },
+
+    removeFromCart: (
+      state,
+      action: PayloadAction<string>
+    ) => {
+      state.items = state.items.filter(
+        (item) =>
+          item.product.id !== action.payload
+      );
+    },
+
+    increaseQuantity: (
+      state,
+      action: PayloadAction<string>
+    ) => {
+      const item = state.items.find(
+        (item) =>
+          item.product.id === action.payload
+      );
+
+      if (item) {
+        item.quantity += 1;
+      }
+    },
+
+    decreaseQuantity: (
+      state,
+      action: PayloadAction<string>
+    ) => {
+      const item = state.items.find(
+        (item) =>
+          item.product.id === action.payload
+      );
+
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+      }
+    },
+
+    clearCart: (state) => {
+      state.items = [];
+    },
+  },
+});
+
+export const {
+  addToCart,
+  removeFromCart,
+  increaseQuantity,
+  decreaseQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
